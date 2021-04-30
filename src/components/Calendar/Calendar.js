f76396f233fab7bcaa2d44b9833513aff2e3fb3a
Tisdale511 <tisdalefry@gmail.com>
2021-04-30 00:17:30 -0400

    previous and next month names change

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -18,18 +18,30 @@ const Calendar = ({events, day, setDay}) => {
         first.getMonth() === second.getMonth() &&
         first.getDate() === second.getDate();
 
-    // console.log(events);
-    const [currentDay] = React.useState(day);
-
+    
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const getNextMonth = () => months[(currentDay.getMonth() + 1) % 12]
-    const getPreviousMonth = () => months[(currentDay.getMonth() + 11) % 12]
+    const [nLabel, setNLabel] = React.useState(months[(new Date().getMonth() + 1) % 12])
+    const [pLabel, setPLabel] = React.useState(months[(new Date().getMonth() + 11) % 12])
+    
+
+    const updateMonthLabels = (date) => {
+        setNLabel(months[(date.getMonth() + 1) % 12]) 
+        setPLabel(months[(date.getMonth() + 11) % 12])
+    }
 
     return (
         // refer to react calendar documentation for the line below
         // runs onClickDay function anytime a day on the calendar is clicked
         // <ReactCalendar onClickDay={(value) => setDay(value)}/>
-        <ReactCalendar prevLabel={`‹ ${getPreviousMonth()}`} nextLabel={`${getNextMonth(day)} ›`} prev2Label={null} next2Label={null} calendarType={'US'} onClickDay={(value) => setDay(value)} tileContent={({ date, view }) => view === 'month' && events.some(event => datesAreOnSameDay(date, new Date(event.startDate))) ? <p>•</p> : <p>&nbsp;</p>
+        <ReactCalendar 
+            onActiveStartDateChange={({ activeStartDate }) => {updateMonthLabels(activeStartDate)}} 
+            prevLabel={`‹ ${pLabel}`} 
+            nextLabel={`${nLabel} ›`} 
+            prev2Label={null} 
+            next2Label={null} 
+            calendarType={'US'}
+            onClickDay={(value) => setDay(value)} 
+            tileContent={({ date, view }) => view === 'month' && events.some(event => datesAreOnSameDay(date, new Date(event.startDate))) ? <p>•</p> : <p>&nbsp;</p>
         }/>
     ); 
 }
